658bacac56a5485ff000f6cf2b0664aeeee0371c
Cody Olsen <stipsan@gmail.com>
2018-07-19 00:42:36 +0200

    netlify cache not working

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,6 @@ module.exports = {
   plugins: [
     'gatsby-plugin-typescript',
     'gatsby-plugin-react-helmet',
-    'gatsby-plugin-netlify-cache',
     {
       resolve: 'gatsby-plugin-styled-components',
       options: { displayName: false },
