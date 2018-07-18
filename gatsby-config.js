441742cc13a3265dfcf6569268999da26747a0b7
Cody Olsen <stipsan@gmail.com>
2018-07-18 23:54:19 +0200

    Remove purgecss for now (#175)
    * Remove purgecss for now
    
    * Update package.json

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,6 @@ module.exports = {
     'gatsby-plugin-typescript',
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-netlify-cache',
-    'gatsby-plugin-purgecss',
     {
       resolve: 'gatsby-plugin-styled-components',
       options: { displayName: false },
